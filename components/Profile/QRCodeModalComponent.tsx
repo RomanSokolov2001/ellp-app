20be3c00eb19c455683bc2c545e038d6b2b54de0
philaptop <mail@jaegerphil.de>
2025-05-06 17:46:18 +0100

    Some changes to QR code modal

--- components/Profile/QRCodeModalComponent.tsx
+++ components/Profile/QRCodeModalComponent.tsx
@@ -1,38 +1,63 @@
-import { useState } from "react";
-import { Modal, Pressable, StyleSheet } from "react-native";
-import QRCode from "react-native-qrcode-svg";
+import { Modal, Pressable, StyleSheet, Text, View } from "react-native";
 import QRCodeComponent from "./QRCodeComponent";
 
 interface QRCodeModalComponentProps {
-    email: string;
-    visibility: boolean;
-    onClose: () => void;
+  email: string;
+  collaborator: string;
+  visibility: boolean;
+  onClose: () => void;
 }
 
 export default function QRCodeModalComponent({ email, visibility, onClose }: QRCodeModalComponentProps) {
-    
-    return (
-        <Modal
-            animationType="fade"
-            transparent={true}
-            visible={visibility}
-            onRequestClose={onClose}
-            >
-            <Pressable style={styles.modalBackground} onPress={onClose}>
-                <QRCodeComponent
-                    email={email}
-                    size={350}
-                />
-            </Pressable>
-        </Modal>
-    );
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={visibility}
+      onRequestClose={onClose}
+    >
+      <Pressable style={styles.modalBackground} onPress={onClose}>
+        <View style={styles.modalContent}>
+          <Text style={styles.header}>Membership Verification</Text>
+          <Text style={styles.instructions}>
+            To redeem a discount, present this QR code to the collaborator/partner shop.
+          </Text>
+          <QRCodeComponent email={email} size={300} />
+        </View>
+      </Pressable>
+    </Modal>
+  );
 }
 
 const styles = StyleSheet.create({
     modalBackground: {
         flex: 1,
-        backgroundColor: 'rgba(0,0,0,0.85)',
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
         justifyContent: 'center',
         alignItems: 'center',
-      },
+    },
+    modalContent: {
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 8,
+        elevation: 8,
+    },
+    header: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#000',
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    instructions: {
+        fontSize: 16,
+        color: '#333',
+        textAlign: 'center',
+        marginBottom: 20,
+    },
 });
